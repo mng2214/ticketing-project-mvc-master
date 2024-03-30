69bc83b6638c96d739d1432a87e77b27c2db4926
mng2214 <mng2214@gmail.com>
2024-03-30 17:59:04 -0500

    Create user controller

--- src/main/java/com/cydeo/controller/LoginController.java
+++ src/main/java/com/cydeo/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
