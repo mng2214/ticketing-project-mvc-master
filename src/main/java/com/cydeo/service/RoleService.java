036f9e68c39a64b5a078aa49631ffc6e8ac0fa09
mng2214 <mng2214@gmail.com>
2024-03-30 19:42:10 -0500

    services added

---
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -0,0 +1,18 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.RoleDTO;
+
+import java.util.List;
+
+public interface RoleService {
+    RoleDTO save(RoleDTO role);
+
+    RoleDTO findById(Long id);
+
+    List<RoleDTO> findAll();
+
+    void delete(RoleDTO role);
+
+    void deleteById(Long id);
+
+}
