942ab67b07e5b11590b757e0c0c68b80e1ea1761
mng2214 <mng2214@gmail.com>
2024-04-01 20:48:42 -0500

    services added

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.Role;
+import com.cydeo.service.RoleService;
+import com.cydeo.service.impl.RoleServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+
+
     @GetMapping("/create")
     public String createUser(Model model) {
         model.addAttribute("user", new UserDTO());
+        model.addAttribute("roles", "");
         return "user/create";
     }
 }
--- src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/RoleServiceImpl.java
@@ -1,4 +1,35 @@
 package com.cydeo.service.impl;
 
-public class RoleServiceImpl {
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.service.RoleService;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+@Controller
+public class RoleServiceImpl implements RoleService {
+
+    @Override
+    public RoleDTO save(RoleDTO object) {
+        return null;
+    }
+
+    @Override
+    public List<RoleDTO> findAll() {
+        return null;
+    }
+
+    @Override
+    public RoleDTO findById(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public void delete(RoleDTO object) {
+
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
 }
