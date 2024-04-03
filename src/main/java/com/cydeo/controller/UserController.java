6d5bb25b3dee034eca396261b391f3e206d5fe54
mng2214 <mng2214@gmail.com>
2024-04-02 20:16:01 -0500

    users controller create.html edited

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.cydeo.controller;
 
+import com.cydeo.bootstrap.DataGenerator;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.entity.Role;
 import com.cydeo.service.RoleService;
+import com.cydeo.service.UserService;
 import com.cydeo.service.impl.RoleServiceImpl;
 import com.cydeo.service.impl.UserServiceImpl;
 import org.springframework.stereotype.Controller;
@@ -15,12 +17,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
-    RoleServiceImpl roleService;
+    RoleService roleService;
+    UserService userService;
+
+    public UserController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
 
     @GetMapping("/create")
     public String createUser(Model model) {
         model.addAttribute("user", new UserDTO());
-       model.addAttribute("roles", roleService.findAll());
-        return "user/create";
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
+        return "/ user/create";
     }
 }
