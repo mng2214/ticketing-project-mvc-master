c6556bfe5e6b54031e18a445c4f7e22786212060
mng2214 <mng2214@gmail.com>
2024-04-11 21:57:33 -0500

    create project user pojo dto

---
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -0,0 +1,19 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.UserDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/project")
+public class ProjectController {
+
+    @GetMapping("/create")
+    public String createProject(Model model) {
+
+        return "/project/create";
+    }
+
+}
