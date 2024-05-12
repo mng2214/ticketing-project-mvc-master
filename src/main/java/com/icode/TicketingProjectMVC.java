4519df159bc039927327767f0778fcfd68f9df6c
mng2214 <mng2214@gmail.com>
2024-05-11 21:46:31 -0500

    Role mapper added

--- src/main/java/com/icode/TicketingProjectMVC.java
+++ src/main/java/com/icode/TicketingProjectMVC.java
@@ -2,12 +2,19 @@ package com.icode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.ui.ModelMap;
 
 @SpringBootApplication
 public class TicketingProjectMVC {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectMVC.class, args);
+    }
+
+    @Bean
+    public ModelMap model() {
+        return new ModelMap();
     }
 
 }
---
+++ src/main/java/com/icode/mapper/RoleMapper.java
@@ -0,0 +1,25 @@
+package com.icode.mapper;
+
+import com.icode.dto.RoleDTO;
+import com.icode.entity.Role;
+import org.modelmapper.ModelMapper;
+
+public class RoleMapper {
+
+    private final ModelMapper modelMapper;
+
+    public RoleMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    // convert to entity
+    public Role convertToEntity(RoleDTO roleDto) {
+        return modelMapper.map(roleDto, Role.class);
+    }
+
+    // convert to dto
+    public RoleDTO convertToDto(Role roleEntity) {
+        return modelMapper.map(roleEntity, RoleDTO.class);
+    }
+
+}
