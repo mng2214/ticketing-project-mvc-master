75b641cb5bed300c75d008da1893fe768d9969a9
mng2214 <mng2214@gmail.com>
2024-05-27 19:08:45 -0500

    security added

--- src/main/java/com/icode/config/SecurityConfig.java
+++ src/main/java/com/icode/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,22 +19,23 @@ import java.util.List;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        List<UserDetails> userList = new ArrayList<UserDetails>();
-        userList.add(new User("admin", encoder.encode("admin"), List.of(new SimpleGrantedAuthority("ROLE_ADMIN"))));
-        userList.add(new User("manager", encoder.encode("manager"), List.of(new SimpleGrantedAuthority("ROLE_MANAGER"))));
-        return new InMemoryUserDetailsManager(userList);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+//        List<UserDetails> userList = new ArrayList<UserDetails>();
+//        userList.add(new User("admin", encoder.encode("admin"), List.of(new SimpleGrantedAuthority("ROLE_ADMIN"))));
+//        userList.add(new User("manager", encoder.encode("manager"), List.of(new SimpleGrantedAuthority("ROLE_MANAGER"))));
+//        return new InMemoryUserDetailsManager(userList);
+//    }
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeRequests()
 
-                .antMatchers("/user/**").hasRole("ADMIN")
-                .antMatchers("/project/**").hasRole("MANAGER")
-                .antMatchers("/task/employee/**").hasRole("EMPLOYEE")
+                .antMatchers("/user/**").hasAuthority("Admin")
+                .antMatchers("/project/**").hasAuthority("Manager")
+                .antMatchers("/task/employee/**").hasAuthority("Employee")
 
                 //   .antMatchers("/task/**").hasAnyAuthority("ROLE_EMPLOYEE")
                 //    .antMatchers("/task/**").hasAnyRole("EMPLOYEE","ADMIN")
@@ -48,13 +50,16 @@ public class SecurityConfig {
                 .anyRequest()
                 .authenticated()
                 .and()
-                //         .httpBasic()
+                //         .h ttpBasic()
                 .formLogin()
                     .loginPage("/login")
                     .defaultSuccessUrl("/welcome")
                     .failureUrl("/login?error=true")
                     .permitAll()
-
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
                 .and()
                 .build();
     }
--- src/main/java/com/icode/entity/User.java
+++ src/main/java/com/icode/entity/User.java
@@ -27,7 +27,7 @@ public class User extends BaseEntity {
 
     private String phone;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @JoinColumn(name = "role_id")
     private Role role;
 
