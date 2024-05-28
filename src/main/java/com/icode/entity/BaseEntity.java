656e8ab3fccb46b205d1bde0b7c6eaad59746273
mng2214 <mng2214@gmail.com>
2024-05-27 21:19:24 -0500

    security grouping

--- src/main/java/com/icode/entity/BaseEntity.java
+++ src/main/java/com/icode/entity/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.icode.entity;
 
 import lombok.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -10,34 +11,22 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    protected Long id;
 
     @Column(updatable = false, nullable = false)
-    private LocalDateTime insertDateTime;
+    protected LocalDateTime insertDateTime;
 
     @Column(updatable = false, nullable = false)
-    private Long insertUserId;
+    protected Long insertUserId;
 
-    private LocalDateTime lastUpdateDateTime;
+    protected LocalDateTime lastUpdateDateTime;
 
-    private Long lastUpdateUserId;
+    protected Long lastUpdateUserId;
 
-    private Boolean isDeleted = false; // soft delete
+    protected Boolean isDeleted = false; // soft delete
 
-    @PrePersist
-    public void onPrePersist() {
-        this.insertDateTime = LocalDateTime.now();
-        this.lastUpdateDateTime = LocalDateTime.now();
-        this.insertUserId = 1L;
-        this.lastUpdateUserId = 1L;
-    }
-
-    @PreUpdate
-    public void onPreUpdate() {
-        this.lastUpdateDateTime = LocalDateTime.now();
-        this.lastUpdateUserId = 1L;
-    }
 }
